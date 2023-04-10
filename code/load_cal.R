df40498ca026fc31d726a77b6a0dee5c3c977a08
PChunark <puttipongchunark@gmail.com>
2023-04-10 14:04:53 +0700

    combine budget load and pdp2022 load

--- code/load_cal.R
+++ code/load_cal.R
@@ -1,15 +1,23 @@
 library(tidyverse) # Package for data manipulation
 library(readxl) #Package for reading excel file. It is not a core package in tidyverse package
 
-b_load <- read_excel("raw_data/load25aug2022andbudegetload.xlsx", skip = 3) %>% 
+## Budget load 20 Oct 2022
+b_load <- 
+  read_excel("raw_data/load25aug2022andbudegetload.xlsx", skip = 3) %>% 
   select(year_th, year, peak_budget20oct2022, ene_budget20oct2022) %>% 
-  drop_na()
-
+  drop_na() %>% 
+  rename(a = peak_budget20oct2022, b = ene_budget20oct2022)  %>% 
+  
+## PDP2022 load BAU ND EE70%
 pdp2022_load <- read_excel("raw_data/load25aug2022andbudegetload.xlsx", skip = 3) %>% 
   select(year_th, year, peak_bau_nd_ee70, ene_bau_nd_ee70) %>% 
   drop_na() %>% 
-  filter(year >= 2029)
+  filter(year >= 2029) %>% 
+  rename(a = peak_bau_nd_ee70, b = ene_bau_nd_ee70)
 
-inner_join(x = b_load, y = pdp2022_load, by = year)
+## Combine 2 loads budget load till 2028 & load PDP2022 from 2029-2030
+newload3u <- rbind(x = b_load, y = pdp2022_load) %>% 
+  rename(newpeak3u = a, newene3u=b)
 
-# "peak_budget20oct2022"="peak_bau_nd_ee70", "ene_budget20oct2022" = "ene_bau_nd_ee70"))
+newload3u %>% 
+  mutate()
