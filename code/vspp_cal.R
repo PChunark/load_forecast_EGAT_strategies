129c7da090af74d540466f7c6374c5049b5a983e
PChunark <puttipongchunark@gmail.com>
2023-04-11 08:26:27 +0700

    Manipulate the vspp data in PDP2022 case7

--- code/vspp_cal.R
+++ code/vspp_cal.R
@@ -34,16 +34,35 @@ vsppenergyPDP2018R1 <-
   # geom_line()
 
 
-read_excel("raw_data/Domeสรุป VSPP_PDP18R1 and ร่าง PDP2022 เคส 7_เพิ่ม vspp.xlsx",
+vsppcontractcapPDP2022C7<-
+  
+  read_excel("raw_data/Domeสรุป VSPP_PDP18R1 and ร่าง PDP2022 เคส 7_เพิ่ม vspp.xlsx",
            sheet = "สรุป VSPP PDP2022 Fuel",
            range = "A1:Q23")%>%
   rename(vspp_mw = "VSPP-Contract Capacity") %>% 
   pivot_longer(-vspp_mw, names_to = "year",values_to = "contract_mw") %>% 
   mutate(year_th = as.numeric(year)+543) %>%
-  pivot_wider(names_from = vspp_mw, values_from = contract_mw) %>% head()
-  select(!รวม) %>% 
+  pivot_wider(names_from = vspp_mw, values_from = contract_mw) %>% 
+  select(!Total) %>% 
   rename_all(tolower) %>% 
-  rename(ind_waste = "industrial waste") %>% 
-  pivot_longer(-year_th&-year, names_to = "fuel",values_to = "vspp_gwh") #%>% 
-# ggplot(aes(x = year, y = vspp_gwh, group = fuel, , color = fuel)) + 
-# geom_line()
+  # rename(ind_waste = "industrial waste") %>% 
+  pivot_longer(-year_th&-year, names_to = "fuel",values_to = "vspp_mw") #%>% 
+  # ggplot(aes(x = as.factor(year), y = vspp_mw, group = fuel, color = fuel)) + 
+  # geom_line()
+
+
+vsppenergyPDP2022C7 <-
+  
+  read_excel("raw_data/Domeสรุป VSPP_PDP18R1 and ร่าง PDP2022 เคส 7_เพิ่ม vspp.xlsx",
+             sheet = "สรุป VSPP PDP2022 Fuel",
+             range = "A25:Q47")%>%
+  rename(vspp_gwh = "VSPP-Generation") %>% 
+  pivot_longer(-vspp_gwh, names_to = "year",values_to = "energy_gwh") %>% 
+  mutate(year_th = as.numeric(year)+543) %>%
+  pivot_wider(names_from = vspp_gwh, values_from = energy_gwh) %>% 
+  select(!Total) %>% 
+  rename_all(tolower) %>% 
+  # rename(ind_waste = "industrial waste") %>% 
+  pivot_longer(-year_th&-year, names_to = "fuel",values_to = "vspp_gwh") %>% 
+  ggplot(aes(x = as.factor(year), y = vspp_gwh, group = fuel, color = fuel)) + 
+  geom_line()
