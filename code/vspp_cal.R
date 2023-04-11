7617e02bd7c765f7c24cff0b87d99956a9255fc9
PChunark <puttipongchunark@gmail.com>
2023-04-11 08:49:30 +0700

    Rename total

--- code/vspp_cal.R
+++ code/vspp_cal.R
@@ -10,9 +10,9 @@ vsppcontractcapPDP2018R1 <-
   pivot_longer(-vspp_mw, names_to = "year_th",values_to = "contract_mw") %>% 
   mutate(year = as.numeric(year_th)-543) %>%
   pivot_wider(names_from = vspp_mw, values_from = contract_mw) %>% 
-  select(!รวม) %>% 
   rename_all(tolower) %>% 
-  rename(ind_waste = "industrial waste") %>% 
+  rename(ind_waste = "industrial waste",
+         total = "รวม") %>% 
   pivot_longer(-year_th&-year, names_to = "fuel",values_to = "contract_mw") #%>% 
   # ggplot(aes(x = year, y = contract_mw, group = fuel, , color = fuel)) + 
   # geom_line()
@@ -26,9 +26,9 @@ vsppenergyPDP2018R1 <-
   pivot_longer(-vspp_gwh, names_to = "year_th",values_to = "energy_gwh") %>% 
   mutate(year = as.numeric(year_th)-543) %>%
   pivot_wider(names_from = vspp_gwh, values_from = energy_gwh) %>% 
-  select(!รวม) %>% 
   rename_all(tolower) %>% 
-  rename(ind_waste = "industrial waste") %>% 
+  rename(ind_waste = "industrial waste",
+         total = "รวม") %>% 
   pivot_longer(-year_th&-year, names_to = "fuel",values_to = "vspp_gwh") #%>% 
   # ggplot(aes(x = year, y = vspp_gwh, group = fuel, , color = fuel)) + 
   # geom_line()
@@ -43,9 +43,7 @@ vsppcontractcapPDP2022C7<-
   pivot_longer(-vspp_mw, names_to = "year",values_to = "contract_mw") %>% 
   mutate(year_th = as.numeric(year)+543) %>%
   pivot_wider(names_from = vspp_mw, values_from = contract_mw) %>% 
-  select(!Total) %>% 
   rename_all(tolower) %>% 
-  # rename(ind_waste = "industrial waste") %>% 
   pivot_longer(-year_th&-year, names_to = "fuel",values_to = "vspp_mw") #%>% 
   # ggplot(aes(x = as.factor(year), y = vspp_mw, group = fuel, color = fuel)) + 
   # geom_line()
@@ -60,9 +58,7 @@ vsppenergyPDP2022C7 <-
   pivot_longer(-vspp_gwh, names_to = "year",values_to = "energy_gwh") %>% 
   mutate(year_th = as.numeric(year)+543) %>%
   pivot_wider(names_from = vspp_gwh, values_from = energy_gwh) %>% 
-  select(!Total) %>% 
   rename_all(tolower) %>% 
-  # rename(ind_waste = "industrial waste") %>% 
   pivot_longer(-year_th&-year, names_to = "fuel",values_to = "vspp_gwh") #%>% 
   # ggplot(aes(x = as.factor(year), y = vspp_gwh, group = fuel, color = fuel)) + 
   # geom_line()
