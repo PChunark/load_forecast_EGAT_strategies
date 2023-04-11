7820708d3495fb0435ad76b501b5ec35ce05bed0
PChunark <puttipongchunark@gmail.com>
2023-04-11 10:53:47 +0700

    Calculate egat net gen

--- code/load_cal.R
+++ code/load_cal.R
@@ -1,12 +1,20 @@
-library(tidyverse) # Package for data manipulation
-library(readxl) #Package for reading excel file. It is not a core package in tidyverse package
+source("code/vspp_cal.R")
+
+# Equations of electricity load in Thailand power system
+## EGT_TOT_SLE[t] = MEA[t] + PEA[t] + DSE[t]
+## EGT_DST[t] = EGT_TOT_SLE[t] + LSS[t]
+## EGT_NET_GEN[t] = EGT_DBT[t] + USE[t] + PUMP[t]
+## NET_GEN_3U[t] = EGT_NET_GEN[t] + VSPP[t]
+## GEN_THA_SYS[t] = NET_GEN_3U[t] + IPS[t]
+
+
 
 ## Budget load 20 Oct 2022
 b_load <- 
   read_excel("raw_data/load25aug2022andbudegetload.xlsx", skip = 3) %>% 
   select(year_th, year, peak_budget20oct2022, ene_budget20oct2022) %>% 
   drop_na() %>% 
-  rename(a = peak_budget20oct2022, b = ene_budget20oct2022)  %>% 
+  rename(a = peak_budget20oct2022, b = ene_budget20oct2022) 
   
 ## PDP2022 load BAU ND EE70%
 pdp2022_load <- read_excel("raw_data/load25aug2022andbudegetload.xlsx", skip = 3) %>% 
@@ -17,7 +25,14 @@ pdp2022_load <- read_excel("raw_data/load25aug2022andbudegetload.xlsx", skip = 3
 
 ## Combine 2 loads budget load till 2028 & load PDP2022 from 2029-2030
 newload3u <- rbind(x = b_load, y = pdp2022_load) %>% 
-  rename(newpeak3u = a, newene3u=b)
+  rename(netGenPeak3U_mw = a, netGenEne3u_gwh=b)
 
 newload3u %>% 
-  mutate()
+  select(!netGenPeak3U_mw) %>% 
+  mutate(vsppenergyPDP2018R1 %>% 
+         filter(year >= 2019,
+               fuel == "total") %>% 
+         select(vspp_gwh),
+         egt_net_gen = netGenEne3u_gwh - vspp_gwh
+         ) 
+  
