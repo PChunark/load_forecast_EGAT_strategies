33e417eb4e22b5ca08da1d5e2b2a7c0cc849f8d7
PChunark <puttipongchunark@gmail.com>
2023-04-19 15:31:37 +0700

    Calculate MEA & PEA electricity sale

--- code/load_cal.R
+++ code/load_cal.R
@@ -45,6 +45,8 @@ newload3u %>%
          egt_dbt = egt_net_gen-total_usepump,
          egt_dbt / (1 + tran_loss_percent %>% filter(year >= 2019) %>% select(percent_loss)),
          newdc_ene %>% select(dc_gwh),
-         mea_pea = percent_loss-dc_gwh
+         mea_pea = percent_loss-dc_gwh,
+         mea_pea * mea_pea_ene %>% select(shr_mea),
+         mea_pea * mea_pea_ene %>% select(shr_pea)
          ) 
   
