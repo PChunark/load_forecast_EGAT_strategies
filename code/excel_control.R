1991b30faf65554e1e7d3cddc0f5eb62a7f65696
PChunark <puttipongchunark@gmail.com>
2023-04-21 11:38:06 +0700

    select growth column

--- code/excel_control.R
+++ code/excel_control.R
@@ -5,4 +5,5 @@ grw_3u <-
   read_excel("process_data/excel_control.xlsx",
            sheet = "grw_3u",
            range = "A1:B83") %>% 
-  drop_na() %>% print(n=100)
+  drop_na() %>% 
+  select(growth)
