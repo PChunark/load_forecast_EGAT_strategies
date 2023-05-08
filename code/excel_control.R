7426221cd3993ce3f2ee2cddd376148d28d6059e
PChunark <puttipongchunark@gmail.com>
2023-05-08 09:47:52 +0700

    Add conditional filter

--- code/excel_control.R
+++ code/excel_control.R
@@ -8,3 +8,19 @@ grw_3u <-
            range = "A1:B83") %>% 
   drop_na() %>% 
   select(growth)
+
+read_excel("process_data/excel_control.xlsx",
+           sheet = "grw_3u",
+           range = "A1:B83") %>% 
+  drop_na() %>% 
+  mutate(year_slc = if_else(growth < 1, 
+                            read_excel("process_data/excel_control.xlsx",
+                                       sheet = "grw_3u",
+                                       range = "A1:B83") %>% 
+                              drop_na() %>% 
+                              mutate(yesr = year - 1), 
+                            read_excel("process_data/excel_control.xlsx",
+                                       sheet = "grw_3u",
+                                       range = "A1:B83") %>% 
+                              drop_na() %>% 
+                              mutate(yesr = year + 1)))
