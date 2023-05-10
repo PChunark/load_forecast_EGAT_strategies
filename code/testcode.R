4581e6b1659e0fcc7c2a778d88d5f282f3fc22b7
PChunark <puttipongchunark@gmail.com>
2023-05-10 15:15:46 +0700

    extract year that the growth rate is applied

--- code/testcode.R
+++ code/testcode.R
@@ -32,49 +32,11 @@ newload3u %>%
            nth(4) %>% 
            pull() * cumulative)
 
-# initial_value <-
-pull(if_else(grw_3u$growth < 1,
-        newload3u %>% 
-          select(!netGenPeak3U_mw) %>% 
-          mutate(x = lag(netGenEne3u_gwh)) 
-          ,F))
-        
-
 newload3u %>% 
-  select(!netGenPeak3U_mw) %>%
-  nth(4) %>% 
-   pull(netGenEne3u_gwh)
-
-
-newload3u %>% 
-  select(!netGenPeak3U_mw) %>%
-  mutate(grw = grw_3u$growth) %>% 
-  ดรสะำพ
-
-
-# https://stackoverflow.com/questions/59778456/using-dplyr-within-groups-select-the-first-value-meeting-a-condition
-set.seed(42)
-
-df <- data.frame(
-  id = sample(LETTERS[1:3], 20, replace = TRUE),
-  time.var = sample(1:20, 20, replace = TRUE),
-  x = sample(c(1:10), 20, replace = TRUE)
-)
-df %>% 
-  arrange(id, time.var) %>% 
-  group_by(id) %>% 
-  mutate(previous.less.5 = tail(c(x[c((x[-n()] < 5), FALSE)]),1)) %>% 
-  group_by(id) %>% 
-  mutate(previous.less.5 = if_else(row_number() == n(), previous.less.5, NULL))
-
-df %>%
-  arrange(id, time.var) %>% 
-  group_by(id) %>%   
-  slice(1:(n()-1)) %>% 
-  filter(x < 5) %>% 
-  slice(n()) %>% 
-  select(-time.var) %>% 
-  right_join(df, ., by="id", suffix =c("",".y")) %>% 
-  group_by(id) %>% 
-  mutate(previous.less.5 = if_else(row_number() == n(), x.y, NULL)) %>%
-  select(-x.y) 
+  select(!netGenPeak3U_mw) %>% 
+  mutate(cumulative = lag(cumprod(grw_3u$growth),n = 0,default = 1)) %>% 
+  mutate(value = newload3u %>% 
+           select(!netGenPeak3U_mw) %>%
+           filter(year == previous_year_grw) %>% 
+           select(netGenEne3u_gwh) %>% 
+           pull() * cumulative)
