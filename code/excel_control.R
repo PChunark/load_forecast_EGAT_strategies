f2ad3d53d8c2c7d98b2a727a551d7aa193bf4a63
PChunark <puttipongchunark@gmail.com>
2023-05-11 07:19:42 +0700

    Add code to apply growth rate to MEA and PEA demand

--- code/excel_control.R
+++ code/excel_control.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(readxl)
 
-## Extract data from excel control sheet
+## Extract growth for electricity from 3 utilities from excel control sheet
 grw_3u_excel <-
   read_excel("process_data/excel_control.xlsx",
              sheet = "grw_3u",
@@ -23,3 +23,45 @@ grw_3u_excel %>%
 pull(year)
 
 ######################################################
+## Extract growth for MEA demand from excel control sheet
+
+grw_mea_dmd_excel <-
+  read_excel("process_data/excel_control.xlsx",
+             sheet = "grw_mea_dmd",
+             range = "A1:B83") %>% 
+  drop_na()
+
+
+grw_mea_dmd <- 
+  grw_mea_dmd_excel %>% 
+  select(growth)
+
+previous_year_mea_dmd_grw <-
+  grw_mea_dmd_excel %>%
+  mutate(years = growth<1 | growth > 1) %>% 
+  filter(years == first(years)) %>% 
+  last() %>% 
+  select(year) %>% 
+  pull(year)
+
+######################################################
+## Extract growth for PEA demand from excel control sheet
+
+grw_pea_dmd_excel <-
+  read_excel("process_data/excel_control.xlsx",
+             sheet = "grw_pea_dmd",
+             range = "A1:B83") %>% 
+  drop_na()
+
+
+grw_pea_dmd <- 
+  grw_pea_dmd_excel %>% 
+  select(growth)
+
+previous_year_pea_dmd_grw <-
+  grw_pea_dmd_excel %>%
+  mutate(years = growth<1 | growth > 1) %>% 
+  filter(years == first(years)) %>% 
+  last() %>% 
+  select(year) %>% 
+  pull(year)
